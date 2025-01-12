9d38a1a0a4ea11a4e6419c4cd6f8bf6dce6992ed
gitdev1 <gitdev1@gmail.com>
2025-01-12 18:39:13 +1300

    add Welcome Page

---
+++ app/src/main/java/com/example/api_with_header/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.api_with_header;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Button btnWelcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        btnWelcome = findViewById(R.id.btn_welcome);
+        btnWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
